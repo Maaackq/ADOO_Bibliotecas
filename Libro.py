0f4b0389a8e256bfd49a8b604a7297ac07f23893
Miguel Varela <maaackq@gmail.com>
2026-02-17 13:01:13 -0600

    Actividad_17feb

--- Libro.py
+++ Libro.py
@@ -1,1 +1,18 @@
-# Libro
+from types import  list,  Optional
+
+class Libro:
+    def __init__(self, isbn: str, titulo: str, autores: str):
+        self.__isbn: str = isbn
+        self.__titulo: str = titulo
+        self.__autores: str = autores 
+        self.__copias: list[Copia] = []
+    
+    def agregar_copia(id_copia: str, self, ubicacion: str)-> Copia:
+        copia = Copias(id_copia, self, ubicacion)
+        self.__copias.append(copia)
+        return copia
+
+
+
+
+    
